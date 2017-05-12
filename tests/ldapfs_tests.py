fd4ab91363d66fba4944ab03726faa7d4d702e78
Michael Gale <michael.gale@pason.com>
2017-05-11 23:00:09 -0400

    Cleaned up unittests setUp and initial tests

--- tests/ldapfs_tests.py
+++ tests/ldapfs_tests.py
@@ -27,7 +27,7 @@ class LDAPFSTestCase(unittest.TestCase):
 
     def _mock_ldapfs_ldap_connect(self):
         """
-        LdapFS have a private method _ldap_connect that handles the ldap connection setup.
+        LdapFS has a private method _ldap_connect that handles the ldap connection setup.
         We mock out that method so we can use the mockldap implementation for testing.
         """
 
@@ -52,12 +52,12 @@ class LDAPFSTestCase(unittest.TestCase):
 
         # Set ldapfs logging level
         ldapfs.log = ldapfs.log_setup(True, True)
-        self.options = ldapfs.parse_options(self.test_options)
+        options = ldapfs.parse_options(self.test_options)
 
         patcher = patch.object(ldapfs.LdapFS, '_ldap_connect', self._mock_ldapfs_ldap_connect)
         self.MockClass = patcher.start()
         self.addCleanup(patcher.stop)
-        self.ldapfsobj = ldapfs.LdapFS(self.options)
+        self.ldapfsobj = ldapfs.LdapFS(options)
 
     def tearDown(self):
         # Stop patching ldap.initialize and reset state.
@@ -65,42 +65,19 @@ class LDAPFSTestCase(unittest.TestCase):
         del self.ldapobj
 
     def test_ldap_connect(self):
-        #self.ldapfsobj = ldapfs.LdapFS(self.options)
         self.assertEquals(self.ldapobj.methods_called(), ['initialize', 'simple_bind_s'])
 
     def test_ldap_search(self):
-        #ldapfsobj = ldapfs.LdapFS(self.options)
-        results = self.ldapfsobj.ldap.search_s('dc=example,dc=com', ldap.SCOPE_ONELEVEL, '(ou=*)')
-        self.assertEquals(sorted(results), sorted([('ou=chemists,dc=example,dc=com', {'objec[848 chars]'})]))
+        results = self.ldapfsobj.ldap.search_s('ou=mathematicians,dc=example,dc=com', ldap.SCOPE_BASE, filterstr='(objectClass=*)')
+        self.assertEquals(len(results), 1)
+        first_hit = sorted(results)[0][0]
+        self.assertEquals(first_hit, 'ou=mathematicians,dc=example,dc=com')
 
     def test_timestamp(self):
         #ldapfsobj = ldapfs.LdapFS(self.options)
         epoch = datetime.datetime.utcfromtimestamp(0)
         results = self.ldapfsobj._convert_timestamp(epoch, "20170511044523.0Z")
         self.assertEquals(results, 1494477923.0)
-
-    #@mock.patch.object(ldapfs.LdapFS, '_ldap_connect', _mock_ldapfs_ldap_connect)
-    #def test_create_base_dn(self):
-    #    ldapfsobj = ldapfs.LdapFS(self.options)
-    #    results = ldapfsobj._create_base_dn()
-
-#    def test_some_ldap(self):
-        """
-        Some LDAP operations, including binds and simple searches, can be
-        mimicked.
-        """
-#        results = _do_simple_ldap_search()
-
-#        self.assertEquals(self.ldapobj.methods_called(), ['initialize', 'simple_bind_s', 'search_s'])
-#        self.assertEquals(sorted(results), sorted(["self.manager", "self.alice"]))
-
-#def _do_simple_ldap_search():
-#    conn = ldap.initialize('ldap://localhost:389')
-#    conn.simple_bind_s('cn=read-only-admin,dc=example,dc=com', 'password')
-#    results = conn.search_s('dc=example,dc=com', ldap.SCOPE_ONELEVEL, '(cn=*)')
-
-#    return results
-
 
 if __name__ == '__main__':
     unittest.main()
